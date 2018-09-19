dc217cc35dcb7418c7bff3d5541aa17329329a9d
Shannon Turner <shannon.v.turner@gmail.com>
2018-09-19 00:21:10 -0400

    Added 17 maps to the gallery, updated a few others

--- images/optimize-images.sh
+++ images/optimize-images.sh
@@ -2,9 +2,9 @@
 
 # Automatically reduces the quality of and resizes PNG images in the specified folder. Requires ImageMagick.
 
-# Usage example: /Users/shannon/Dropbox/github/metro-map-maker/images/optimize-images.sh /Users/shannon/Dropbox/github/metro-map-maker/images/unoptimized/
+# Usage example: /Users/shannon/Dropbox/github/metro-map-maker/images/optimize-images.sh /Users/shannon/Dropbox/github/metro-map-maker/images/___unoptimized/
 
-# Note: To avoid optimizing an image more than once, begin in the "unoptimized" folder and once optimization has occurred, if the results are good, move the image into the parent images folder
+# Note: To avoid optimizing an image more than once, begin in the "___unoptimized" folder and once optimization has occurred, if the results are good, move the image into the parent images folder
 
 for i in ${1%/*}/*.png;
 do
