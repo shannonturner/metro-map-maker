0598a2f4473238c6e8dcbcc12d9acb41d265b5a1
Shannon Turner <shannon.v.turner@gmail.com>
2018-10-07 21:43:10 -0400

    Added a test that will run each (existing) map through the validator. If it passes, I know that any changes I make to the validator did not cause any of the 3000-odd real maps I have right now to fail, giving me some confidence that my changes to the validator are not breaking changes

--- metro_map_saver/map_saver/tests.py
+++ metro_map_saver/map_saver/tests.py
@@ -1,6 +1,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from map_saver.models import SavedMap
+from map_saver.validator import validate_metro_map
+
+from django.core.management import call_command
 from django.test import TestCase
 
-# Create your tests here.
+class ValidateMapTestCase(TestCase):
+
+    fixtures = ['untracked/backup-20180913-dates-backported.json']
+
+    def test_fixtures_loaded(self):
+        self.assertEqual(SavedMap.objects.count(), 2065)
+
+    def test_validator(self):
+
+        """ Ensure that all maps already saved will pass the validation
+              as written in validate_metro_map()
+            That way, I'll know any changes to validate_metro_map() have
+              at least been tested on a large number of real, existing maps.
+        """
+
+        saved_maps = SavedMap.objects.all()
+        for saved_map in saved_maps:
+            saved_map = saved_map.mapdata.replace("u'", "'").replace("'", '"').strip('"').strip("'").replace('\\', '\\\\')
+            validate_metro_map(saved_map)
+
+    def test_invalid_toomanylines(self):
+
+        """ Reject a map that has too many lines (Validation #04B: <= 100 lines)
+        """
+        pass
