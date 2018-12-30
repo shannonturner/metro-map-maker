91aaf988ab555a99e4f769ed3d21db2ca99a9fe7
Shannon Turner <shannon.v.turner@gmail.com>
2018-12-30 11:27:43 -0500

    Added cheat code: combineMap which smushes two maps together

--- js/metromapmaker.js
+++ js/metromapmaker.js
@@ -1550,6 +1550,42 @@ function enableRightClick() {
   document.getElementById('grid').removeEventListener('contextmenu', disableRightClick);
 } // enableRightClick()
 
+function combineMap(urlhash) {
+  // Add the map at the urlhash to the existing map.
+  // Existing map must not be overwritten by the new map.
+  // I expect this will mostly be used to bring terrain into an existing map
+  // but this will only work for maps that are exactly aligned, or they will look a bit silly
+  $.get('https://metromapmaker.com/save/' + urlhash).done(function (savedMapData) {
+    savedMapData = savedMapData.replaceAll('u&#39;', '"').replaceAll('&#39;', '"').replaceAll('\\\\x', '&#x');
+    if (savedMapData.replace(/\s/g,'').slice(0,7) == '[ERROR]') {
+      console.log("[WARN] Can't combine that map!");
+    } else {
+      savedMapData = JSON.parse(savedMapData)
+
+      for (var x in savedMapData) {
+        for (var y in savedMapData[x]) {
+          if (!activeMap[x]) {
+            activeMap[x] = {y: savedMapData[x][y]}
+          } else if (activeMap[x] && !activeMap[x][y]) {
+            activeMap[x][y] = savedMapData[x][y]
+          }
+        } // for y
+      } // for x
+
+      // Must also add the globals, otherwise the map probably won't be saveable
+      for (var line in savedMapData["global"]["lines"]) {
+        if (!activeMap["global"]["lines"][line]) {
+          activeMap["global"]["lines"][line] = savedMapData["global"]["lines"][line]
+        }
+      }
+
+      getMapSize(activeMap);
+      loadMapFromObject(activeMap); // Must load not with update in order to update the map lines
+      drawCanvas();
+    }
+  });
+} // combineMap(urlhash)
+
 // Steer mobile users toward the gallery, for a better experience
 $('#try-on-mobile').click(function() {
   $('#try-on-mobile').hide();
