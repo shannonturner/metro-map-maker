cadc8e4934897565c58c6154b6fd5979013df3a1
Shannon Turner <shannon.v.turner@gmail.com>
2020-01-11 00:38:18 -0500

    Show number of maps currently tagged for maps matching the name suggested by CitySuggester

--- metro_map_saver/map_saver/templatetags/admin_gallery_tags.py
+++ metro_map_saver/map_saver/templatetags/admin_gallery_tags.py
@@ -17,3 +17,17 @@ def existing_maps(value, arg):
         'name': value.name.split("(")[0].strip(),
         'tags__slug': arg,
     }).count()
+
+@register.simple_tag(name='find_existing_maps')
+def existing_maps_search_by_name_tag(name, tag):
+
+    """ Use to find existing publicly visible maps,
+        searching by name and tag
+    """
+    name = name.split("(")[0].strip()
+    if name:
+        return SavedMap.objects.filter(**{
+            'publicly_visible': True,
+            'name__contains': name,
+            'tags__slug': tag,
+        }).count()
