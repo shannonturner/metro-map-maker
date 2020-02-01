e0360e74169ec8471def16d9080f34d8338357ce
Shannon Turner <shannon.v.turner@gmail.com>
2020-02-01 15:48:46 -0500

    City suggester should show the numbers of its existing maps in the admin gallery even if there are commas in its name

--- metro_map_saver/map_saver/templatetags/admin_gallery_tags.py
+++ metro_map_saver/map_saver/templatetags/admin_gallery_tags.py
@@ -9,12 +9,14 @@ def existing_maps(value, arg):
     """ Use to find existing maps with the same name as this
         that are publicly visible and have some tag
     """
-
+    # user-named maps have suggested tags in parentheses,
+    #   and TravelSystems contain the country name (after a comma)
+    # we don't want these considered part of the name
+    name = value.name.split("(")[0].strip()
+    name = name.split(",")[0].strip()
     return SavedMap.objects.filter(**{
         'publicly_visible': True,
-        # user-named maps have suggested tags in parentheses,
-        # we don't want these considered part of the name
-        'name': value.name.split("(")[0].strip(),
+        'name': name,
         'tags__slug': arg,
     }).count()
 
@@ -25,6 +27,7 @@ def existing_maps_search_by_name_tag(name, tag):
         searching by name and tag
     """
     name = name.split("(")[0].strip()
+    name = name.split(",")[0].strip()
     if name:
         return SavedMap.objects.filter(**{
             'publicly_visible': True,
