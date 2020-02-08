2af5b359ec6ea82d4a6b0ad820c94a3ce89e3b95
Shannon Turner <shannon.v.turner@gmail.com>
2020-02-08 07:59:23 -0500

    Updated memcached location

--- metro_map_saver/metro_map_saver/settings.py
+++ metro_map_saver/metro_map_saver/settings.py
@@ -134,7 +134,7 @@ DATABASES = {
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
+        'LOCATION': 'unix:/home/sturner/memcached.sock',
     }
 }
 
