cc2c24ca8a63d44df96edab9473f448dc13fca76
Shannon Turner <shannon.v.turner@gmail.com>
2024-03-05 18:49:00 -0500

    add test for circles-lg

--- metro_map_saver/map_saver/tests/station_marker.py
+++ metro_map_saver/map_saver/tests/station_marker.py
@@ -14,7 +14,6 @@ class StationMarkerTest(TestCase):
         if xfer and line size >= 0.5, circle sm/md have a big white circle and small color circle
         elif xfer, circle sm/md have 1 color circle
         else, circle sm/md have a big color circle and small white circle
-        circles lg: as wmata, but with color
         circles-thin: always b/w; xfer controls stroke width
         rectangles:
             if line size >= 0.5, drawn in b/w; otherwise color
@@ -23,19 +22,27 @@ class StationMarkerTest(TestCase):
         connected stations
         """
 
-    def test_wmata(self):
+    def test_wmata(self, line_color='bd1038', station_color='#000', style='wmata'):
 
         """ Confirm the styling of WMATA (Classic) stations
         """
 
-        station = {'xy': (1,1), 'style': 'wmata', 'color': '#bd1038'}
+        station = {'xy': (1,1), 'style': style, 'color': line_color}
         marker = station_marker(station, 'wmata', 1, {}, {})
         self.assertEqual(marker.count('<circle cx="1" cy="1"'), 2)
         self.assertEqual(marker.count('#fff'), 1)
-        self.assertEqual(marker.count('#000'), 1)
+        self.assertEqual(marker.count(station_color), 1)
 
         station['transfer'] = 1
         marker = station_marker(station, 'wmata', 1, {}, {})
         self.assertEqual(marker.count('<circle cx="1" cy="1"'), 4)
         self.assertEqual(marker.count('#fff'), 2)
-        self.assertEqual(marker.count('#000'), 2)
+        self.assertEqual(marker.count(station_color), 2)
+
+    def test_circles_lg(self):
+
+        """ circles-lg is really just WMATA styling but
+            with the line color instead
+        """
+
+        self.test_wmata(line_color='bd1038', station_color='#bd1038', style='circles-lg')
