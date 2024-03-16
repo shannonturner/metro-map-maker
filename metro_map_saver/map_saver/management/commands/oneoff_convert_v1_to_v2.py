c123b9c3703843cd58b35b7b2a5824578edca0ea
Shannon Turner <shannon.v.turner@gmail.com>
2024-03-16 16:44:33 -0400

    dont use paginator -- there is some weird behavior where it skips pages that I havent been able to figure out

--- metro_map_saver/map_saver/management/commands/oneoff_convert_v1_to_v2.py
+++ metro_map_saver/map_saver/management/commands/oneoff_convert_v1_to_v2.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand
-from django.core.paginator import Paginator
 
 import json
+import time
+
+CHUNK_SIZE = 1000
 
 class Command(BaseCommand):
     help = """
@@ -11,12 +13,28 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '-c',
-            '--chunksize',
+            '-s',
+            '--start',
             type=int,
-            dest='chunksize',
-            default=1000,
-            help='Generate mapdata for this many maps at a time (but they will all be processed)',
+            dest='start',
+            default=0,
+            help='(Re-)calculate data for maps starting with this PK.',
+        )
+        parser.add_argument(
+            '-e',
+            '--end',
+            type=int,
+            dest='end',
+            default=0,
+            help='Process maps with a PK lower than this value. Does NOT process maps that already have v2 data.',
+        )
+        parser.add_argument(
+            '-l',
+            '--limit',
+            type=int,
+            dest='limit',
+            default=CHUNK_SIZE * 5,
+            help='Only calculate images and thumbnails for this many maps at once.',
         )
         parser.add_argument(
             '-u',
@@ -30,29 +48,47 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         from map_saver.models import SavedMap
 
-        chunksize = kwargs.get('chunksize')
         urlhash = kwargs.get('urlhash')
+        start = kwargs['start']
+        end = kwargs['end']
+        limit = kwargs['limit']
 
         if urlhash:
-            maps_to_update = SavedMap.objects.filter(urlhash=urlhash)
+            limit = total = 1
+            maps_to_update = SavedMap.objects.filter(urlhash=urlhash).order_by('id')[:limit]
+        elif start or end:
+            start = start or 1
+            end = end or (start + limit + 1)
+            maps_to_update = SavedMap.objects.filter(pk__in=range(start, end))[:limit]
         else:
-            maps_to_update = SavedMap.objects.filter(data={})
-        count = maps_to_update.count()
+            maps_to_update = SavedMap.objects.filter(data={}).order_by('id')[:limit]
 
-        for page in Paginator(maps_to_update.order_by('id'), chunksize):
+        total = maps_to_update.count()
+        self.stdout.write(f'Generating v2 mapdata for {total} maps')
+        count = 0
+        t0 = time.time()
 
-            if chunksize >= 1000:
-                self.stdout.write(f"Generating v2 mapdata for {page.start_index()} - {page.end_index()} (of {count})")
-
-            for saved_map in page.object_list:
-                try:
-                    saved_map.convert_mapdata_v1_to_v2()
-                except json.decoder.JSONDecodeError:
-                    print(f'[WARN] JSONDecodeError for {saved_map.urlhash}') # Will need to manually fix these
+        for saved_map in maps_to_update:
+            try:
+                saved_map.convert_mapdata_v1_to_v2()
+            except json.decoder.JSONDecodeError:
+                if not saved_map.mapdata:
+                    # These are test maps that started in v2 made in dev/staging
+                    #   and don't need to be converted, but this is being run
+                    #   after these maps were made
+                    self.stdout.write(f'Skipping dev/staging map {saved_map.urlhash}')
                     continue
-                except Exception as exc:
-                    print(f'[WARN] Exception {exc} for {saved_map.urlhash}')
-                    continue
+                self.stdout.write(f'[WARN] JSONDecodeError for {saved_map.urlhash})') # Will need to manually fix these
+                continue
+            except Exception as exc:
+                self.stdout.write(f'[WARN] Exception {exc} for {saved_map.urlhash}')
+                continue
+            else:
+                self.stdout.write(f'Generated v2 data for #{saved_map.id}: {saved_map.urlhash} ({saved_map.created_at.date()})')
 
-                if chunksize < 1000:
-                    self.stdout.write(f"Generated v2 mapdata for {saved_map.urlhash}")
+            count += 1
+            if count >= limit:
+                break
+
+        t1 = time.time()
+        self.stdout.write(f'Finished in {(t1 - t0)}s')
