3a350d3c5ac61025aeddf8791ce2bba474cdf981
Shannon Turner <shannon.v.turner@gmail.com>
2024-11-16 03:25:47 -0500

    forgot this one

--- metro_map_saver/map_saver/static/js/metromapmaker.js
+++ metro_map_saver/map_saver/static/js/metromapmaker.js
@@ -62,7 +62,7 @@ compatibilityModeIndicator()
 
 const numberKeys = ['Digit1','Digit2','Digit3','Digit4','Digit5','Digit6','Digit7','Digit8','Digit9','Digit0', 'Digit1','Digit2','Digit3','Digit4','Digit5','Digit6','Digit7','Digit8','Digit9','Digit0', 'Digit1','Digit2','Digit3','Digit4','Digit5','Digit6','Digit7','Digit8','Digit9','Digit0'] // 1-30; is set up this way to have same functionality on all keyboards
 const ALLOWED_LINE_WIDTHS = [100, 75.0, 50.0, 25.0, 12.5]
-const ALLOWED_LINE_STYLES = ['solid', 'dashed', 'dashed_uneven', 'dashed_square', 'dashed_outline', 'dense_thin', 'dense_thick', 'dotted_dense', 'dotted', 'dotted_outline', 'hollow', 'hollow_open']
+const ALLOWED_LINE_STYLES = ['solid', 'dashed', 'dashed_uneven', 'dashed_square', 'dashed_outline', 'dense_thin', 'dense_thick', 'dotted_dense', 'dotted', 'dotted_outline', 'densedot_outline', 'hollow', 'hollow_open']
 const ALLOWED_ORIENTATIONS = [0, 45, -45, 90, -90, 135, -135, 180, 1, -1];
 const ALLOWED_STYLES = ['wmata', 'rect', 'rect-round', 'circles-lg', 'circles-md', 'circles-sm', 'circles-thin']
 const ALLOWED_SIZES = [80, 120, 160, 200, 240, 360]
